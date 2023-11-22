138e2ab576ad0956c34f69c5ce863096bc09ce96
Sreerag Mohan <sreeraagmohancv@gmail.com>
2023-11-22 18:36:06 +0530

    added input fields and hooks added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import img from '../assets/investment-calculator-logo.png'
+import React from "react";
+import img from "../assets/investment-calculator-logo.png";
 
 function Header() {
   return (
-    <header id='header'>
-        <img src={img} alt="" />
-      <h1>React Investment Calculator</h1>
+    <header id="header">
+      <img src={img} alt="" />
+      <h1>Investment Calculator</h1>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -1,26 +1,47 @@
-import React from 'react'
+import React, { useState } from "react";
 
 function UserInput() {
+
+  const [value, setValue] = useState({
+    initialInvestment: 10000,
+    anuualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 1
+  })
+
+  const handleChange = (inputIdentifier, newValue) => {
+    setValue( (prevValue) => {
+      return {
+        ...prevValue,
+        [inputIdentifier]: newValue
+      }
+    })
+  }
+
   return (
-    <section id='user-input'>
-      <div className='input-group'>
-        <label htmlFor="">Initial Investment</label>
-        <input type="text" />
+    <section id="user-input">
+      <div className="input-group">
+        <p>
+          <label htmlFor="">Initial Investment</label>
+          <input type="number" value={value.initialInvestment} onChange={(e) => handleChange('initialInvestment', e.target.value )} required />
+        </p>
+        <p>
+          <label htmlFor="">Annual Investment</label>
+          <input type="number" value={value.anuualInvestment} onChange={(e) => handleChange('annualInvestment', e.target.value )} required />
+        </p>
       </div>
-      <div className='input-group'>
-        <label htmlFor="">Annual Investment</label>
-        <input type="text" />
-      </div>
-      <div className='input-group'>
-        <label htmlFor="">Expected Return</label>
-        <input type="text" />
-      </div>
-      <div className='input-group'>
-        <label htmlFor="">Duration</label>
-        <input type="text" />
+      <div className="input-group">
+        <p>
+          <label htmlFor="">Expected Return</label>
+          <input type="number" value={value.expectedReturn} onChange={(e) => { console.log(e.target.value), handleChange('expectedReturn', e.target.value )}} required />
+        </p>
+        <p>
+          <label htmlFor="">Duration</label>
+          <input type="number" value={value.duration} onChange={(e) => handleChange('duration', e.target.value )} required />
+        </p>
       </div>
     </section>
-  )
+  );
 }
 
-export default UserInput
+export default UserInput;
