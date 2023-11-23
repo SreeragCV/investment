aae570e8765e8a88576df936b2b5ba2ef6dc45c9
Sreerag Mohan <sreeraagmohancv@gmail.com>
2023-11-23 13:27:16 +0530

    error handled

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,8 @@ function App() {
     duration: 10,
   });
 
+  const inputIsValid = userInput.duration >= 1; 
+
   const handleChange = (inputIdentifier, newValue) => {
     setUserInput((prevValue) => {
       return {
@@ -24,7 +26,8 @@ function App() {
     <>
       <Header />
       <UserInput userInput={userInput} onChange={handleChange} />
-      <Results input={userInput} />
+      {!inputIsValid && <p className="center">ENTER A VALID INPUT DATA!</p>}
+      {inputIsValid && <Results input={userInput} />}
     </>
   );
 }
